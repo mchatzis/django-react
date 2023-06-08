24fabdc0ed7552f3e428d3d3a58032905f817685
Michail Chatzis <michatzis@gmail.com>
2023-06-08 14:52:05 +0300

    ignored static directory and changed to SwaggerView

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from api.views import EmployeeViewSet
 from rest_framework.routers import DefaultRouter
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register('employees', EmployeeViewSet, basename='employees')
@@ -9,6 +9,7 @@ router.register('employees', EmployeeViewSet, basename='employees')
 app_name = 'api'
 
 urlpatterns = [
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', include(router.urls)),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/doc/', SpectacularSwaggerView.as_view(url_name='api:schema'), name='doc'),
 ]
